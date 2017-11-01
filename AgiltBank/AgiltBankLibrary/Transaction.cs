a04f42fca309d4707a6a5d6b7dbef4f9bc529850
Perly Gustafsson <contact@daperley.com>
2017-11-01 11:24:16 +0100

    Transaction class and skeleton added

---
+++ AgiltBank/AgiltBankLibrary/Transaction.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgiltBankLibrary
+{
+    class Transaction
+    {
+        // TODO: Update with the right var for the id's 
+        // TODO: Update with logic when the account class is done
+
+        public bool Insert(int accountId, decimal amount)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool Extract(int accountId, decimal amount)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool Transfer(int fromAccountId, int toAccountId, decimal amount)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
