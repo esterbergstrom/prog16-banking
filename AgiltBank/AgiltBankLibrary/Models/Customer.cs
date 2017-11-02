ed9e6bef2cdcd146a2986090ae9685e3cdef8a25
Emil Ekman <tangooe@gmail.com>
2017-11-02 15:06:51 +0100

    Added Account & Customer Model

---
+++ AgiltBank/AgiltBankLibrary/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace AgiltBankLibrary.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string OrganisationNumber { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public ICollection<Account> Accounts { get; set; }
+    }
+}
