dfcacf5548d4c19444157dc27fe70fda37326cd6
Emil Ekman <tangooe@gmail.com>
2017-11-02 15:20:03 +0100

    Added BankData Class

---
+++ AgiltBank/AgiltBankLibrary/Data/BankData.cs
@@ -0,0 +1,18 @@
+﻿using AgiltBankLibrary.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AgiltBankLibrary.Data
+{
+    public class BankData
+    {
+        public BankData(IEnumerable<Customer> customers, IEnumerable<Account> accounts)
+        {
+            Customers = customers.ToList();
+            Accounts = accounts.ToList();
+        }
+
+        public IList<Customer> Customers { get; }
+        public IList<Account> Accounts { get; }
+    }
+}
