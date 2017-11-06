aabc80c788ab2bca2568bc60def14c0b7ed8ca36
Ester Bergström <ester.d.bergstrom@gmail.com>
2017-11-06 14:26:02 +0100

    Add an abstract state class with logic

---
+++ AgiltBank/AgiltBank/Context.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgiltBank
+{
+    public class Context
+    {
+        public State State { get; set; }
+    }
+}
---
+++ AgiltBank/AgiltBank/State.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgiltBank
+{
+    public abstract class State
+    {
+        public delegate void MenuItemHandler();
+
+        protected string _prompt = string.Empty;
+        protected Dictionary<string, MenuItemHandler> _menuItems = new Dictionary<string, MenuItemHandler>();
+
+        protected void Read()
+        {
+            Console.Write($"{_prompt}: ");
+
+            if (_menuItems.Count < 1)
+            {
+                var line = Console.ReadLine();
+                ProcessLine(line);
+            }
+            else
+            {
+                var key = Console.ReadKey().ToString();
+
+                if (_menuItems.ContainsKey(key))
+                {
+                    _menuItems[key]();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please try again.");
+                    Read();
+                }
+            }
+        }
+
+        protected abstract void ProcessLine(string line);
+    }
+}
