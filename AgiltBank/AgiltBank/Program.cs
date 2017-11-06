2d7b0c4f683e2f87e3474826e7af6fc562d1d2aa
Ester Bergström <ester.d.bergstrom@gmail.com>
2017-11-06 14:27:42 +0100

    Remove previous abstractions

--- AgiltBank/AgiltBank/Program.cs
+++ AgiltBank/AgiltBank/Program.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using AgiltBank.Frontend;
 
 namespace AgiltBank
 {
     class Program
     {
-        public static StateContext StateContext { get; set; }
-
         static void Main(string[] args)
         {
-            StateContext = new StateContext();
-
             Console.WriteLine("Hello World!");
         }
     }
